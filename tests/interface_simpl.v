c3d2ab50dc24463d94ca917be82f9a28a4438c33
cecilemarcon <cecilemarcon02@gmail.com>
2026-06-04 13:51:01 +0200

    back to a good test file

--- tests/interface_simpl.v
+++ tests/interface_simpl.v
@@ -17,48 +17,63 @@ HB.mixin Record isB T := {
 
 HB.structure Definition B := {T of isB T}.
 
+
+
 (* Interface should behave as a mixin *)
-HB.interface Record isSemiGroup T := {
+HB.interface Record SemiGroup T := {
     op : T -> T -> T;
     opA : forall x y z, op x (op y z) = op (op x y) z
   }.
 
-HB.structure Definition SemiGroup := {T of isSemiGroup T}.
+HB.structure Definition SemiGroupS := {T of SemiGroup T}.
+HB.instance  Definition _ := SemiGroup.Build Z Z.add Z.add_assoc.
 
-
-HB.instance  Definition _ := isSemiGroup.Build Z Z.add Z.add_assoc.
-
-Lemma lestfassoc (T : SemiGroup.type) (x y z : T) : op (op x y) z = op x (op y z).
+Lemma lestfassoc (T : SemiGroupS.type) (x y z : T) : op (op x y) z = op x (op y z).
   Proof. symmetry. apply opA. Qed.
 
 
 (* Building a mixin dependent on a previous one *)
-HB.interface Record isGroup T of isSemiGroup T := {
+HB.interface Record isGroup T of SemiGroup T := { 
     e : T;
     idl : forall x, op e x = x;
     idr : forall x, op x e = x;
     invl : forall x, exists xinv, op x xinv = e;
     invr : forall x, exists xinv, op xinv x = e;
 }.
+
+
 (* Structure requires the two mixins *)
-HB.structure Definition Group := {T of isSemiGroup T & isGroup T}.
+HB.structure Definition Group := {T of SemiGroup T & isGroup T}.
 
 
 
-
-(* Interface should behave as a factory *)
-#[alternative] HB.interface Record isGroup T := {
-    op : T -> T -> T;
-    opA' : forall x y z, op (op x y) z = op x (op y z);
-    e : T;  
-    idl' : forall x, op e x = x;
-    idr : forall x, op x e = x;
-    invl : forall x, exists xinv, op x xinv = e;
-    invr : forall x, exists xinv, op xinv x = e; 
-    default : T
+(* Building a mixin dependent on a previous one *)
+HB.interface Record ComGroup T of Group T := { (*of SemiGroup T*)
+    opC : forall x y:T, op x y = op y x;
 }.
 
-HB.builders Context T of isGroupFACT T.
-HB.instance Definition _ := isSemiGroup.Build T op (fun _ _ _ => eq_sym (opA' _ _ _)).
+HB.structure Definition ComGroupS := {T of ComGroup T &}.
+
+
+
+#[alternative] HB.interface Record ComGroup T  of SemiGroup T:= { (*of SemiGroup T*)
+    opC : forall x y:T, op x y = op y x;
+    e : T;
+    idr : forall x, op x e = x;
+    invr : forall x, exists xinv, op xinv x = e;
+}.
+
+
+HB.builders Context T of ComGroupFACT T.
+(* assumption *)
+Lemma invl : forall x, exists xinv, op x xinv = e.
+  intros. destruct (invr x). exists x0. rewrite opC. auto. Qed.
+Lemma idl : forall x, op e x = x. 
+    intros. rewrite opC. apply idr. Qed. 
+
+HB.instance Definition _ := isGroup.Build T e idl idr invl invr.
+HB.instance Definition _ := ComGroup.Build T opC.
 
 HB.end.
+
+
